3701bd31d5164c964fec5659559d4008e17248dd
Gary Rennie <gazler@gmail.com>
2017-07-27 17:26:13 +0100

    Example of server push

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -8,6 +8,13 @@ defmodule PhoenixCowboy2Example.PageController do
         _                          -> false
       end
 
+    Plug.Conn.H2.push(conn, "/css/app.css", [{"accept", "text/css"}])
+    Plug.Conn.H2.push(conn, "/js/app.js", [{"accept", "application/javascript"}])
+    Plug.Conn.H2.push(conn, "/images/chart.png", [{"accept", "image/png"}])
+    Plug.Conn.H2.push(conn, "/images/book.jpg", [{"accept", "image/jpeg"}])
+    Plug.Conn.H2.push(conn, "/images/unicorn.jpg", [{"accept", "image/jpeg"}])
+    Plug.Conn.H2.push(conn, "/images/magic.jpg", [{"accept", "image/jpeg"}])
+
     render conn, "index.html", http2: http2
   end
 end
